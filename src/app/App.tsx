c9940221a88225a078b9c60925b7be449604a83f
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-05 09:05:51 +0900

    BRR: INITIAL COMMIT

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../public/icons/logo.svg';
+
+import '../css/App.css';
+
+function App() {
+  return (
+    <div>Home</div>
+  );
+}
+
+export default App;
