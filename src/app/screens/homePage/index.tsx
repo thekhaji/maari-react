6e48c5a74090782f6a616c7106da3ed82eaaf968
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-09 20:23:34 +0900

    fix: develop screen based components

---
+++ src/app/screens/homePage/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export function homePage() {
+    return <Container>homePage</Container>
+}
---
+++ src/app/screens/ordersPage/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export function ordersPage() {
+    return <Container>ordersPage</Container>
+}
