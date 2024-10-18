fcaf8ba05c7da04dd774d1b16649410626587acb
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-19 02:26:59 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,8 +5,25 @@ import Products from "../productsCard/Products";
 import PopularProducts from "./PopularProducts";
 import { useEffect } from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import {createSelector} from "reselect" ;
+import {setProducts} from "./slice" ;
+import { retrieveProduct } from "./selector";
+import { Product } from "../../../libs/types/product";
+
+const actionDispatch = (dispatch: Dispatch) => ({
+    setProducts: (data: Product[]) => dispatch(setProducts(data)),
+});
+
+const productRetriever = createSelector(
+    retrieveProduct,
+    (product) => ({product})
+);
+
 export function HomePage() {
-    // Selector: Strore => Data
+    const {setProducts} = actionDispatch(useDispatch());
+    const {product} = useSelector(productRetriever);
 
     useEffect(()=>{
         //Backend server data request => Data
