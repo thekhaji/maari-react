c0986e6c2d43095101a3a2b0eece0b52f957ccbd
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-10-19 12:11:42 +0900

    feat: develop Product component logic

--- src/app/screens/ProductsCard/Products.tsx
+++ src/app/screens/ProductsCard/Products.tsx
@@ -1,16 +1,58 @@
-import React, { useEffect } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import { Container } from "@mui/material";
+import { Link, NavLink } from "react-router-dom";
+import { Container, PaginationItem } from "@mui/material";
 import { ProductCollection, ProductStatus } from "../../../libs/enums/product.enum";
-
+import Pagination from "@mui/material/Pagination";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
 import { setProducts } from "../productsPage/slice";
 import {createSelector} from "reselect";
 import { retrieveProducts } from "../productsPage/selector";
-import { Product } from "../../../libs/types/product";
+import { Product, ProductInquiry } from "../../../libs/types/product";
 import ProductService from "../../services/ProductService";
+import { Search } from "@mui/icons-material";
+
+const headerItemsArr =[
+  {
+    id:1,
+    itemNamme:"Декоративная косметика",
+    path: ProductCollection.MAKEUP,
+  },
+  {
+    id:2,
+    itemNamme:"Для волос",
+    path:ProductCollection.HAIR,
+  },
+  {
+    id:3,
+    itemNamme:"Для лица",
+    path:ProductCollection.FACE,
+  },
+  {
+    id:4,
+    itemNamme:"Для тела",
+    path:ProductCollection.BODY,
+  },
+  {
+    id:5,
+    itemNamme:"Детская косметика",
+    path:ProductCollection.KIDS,
+  },
+  {
+    id:6,
+    itemNamme:"Для мужчин",
+    path:ProductCollection.MEN,
+  },
+  {
+    id:7,
+    itemNamme:"Аксессуары",
+    path:ProductCollection.ACCESSORY,
+  }
+]
 
 /** REDUX SLICE AN SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
@@ -23,27 +65,66 @@ const productsRetriever = createSelector(retrieveProducts, (products) => ({
 function Products(){
   const {setProducts} = actionDispatch(useDispatch());
   const {products} = useSelector(productsRetriever);
+  const [productSearch, setProductSearch] = useState<ProductInquiry>({
+      page: 1,
+      limit: 8,
+      order: 'createdAt',
+      search: "",
+  });
+
   useEffect(()=>{
     const product = new ProductService();
-    product.getProucts({
-      page: 1,
-      limit: 9,
-      order: 'createdAt',
-    })
+    product.getProucts(productSearch)
     .then((data) => setProducts(data))
     .catch((err) => console.log(err));
-  },[]);
+  },[productSearch]);
+
+  /** HANDLERS **/
+  const searchCollectionHandler = (collection: ProductCollection) => {
+    productSearch.page = 1;
+    productSearch.productCollection = collection;
+    setProductSearch({ ...productSearch });
+  };
+
+  const paginationHanler = (e: ChangeEvent<any>, value: number) => {
+    productSearch.page = value;
+    setProductSearch({ ...productSearch });
+  }
 
   return (
     <div className={""}>
-      <div className={"products-wrapper"}>
-
-      {
-        products.map((item)=>
-          <ProductCard item={item}/>
-        )
-      }
+      <div className={"sub-wrapper"}>
+        <div className="">
+            <ul className={"sub-subheaderList"}>       
+                {
+                  headerItemsArr.map((item)=>
+                    <li className={"sub-subheaderItem"}>
+                        <div className={"sub-linkBox"} >
+                          <button 
+                          className={"sub-subLinks"}
+                          color={productSearch.productCollection === item.path ? "white" : "black"} 
+                          onClick={()=>searchCollectionHandler(item.path)}>
+                            {item.itemNamme}</button>
+                        </div>
+                    </li>
+                  )
+                }
+            </ul>
+        </div>
       </div>
+      <div className={"products-wrapper"}>
+        {
+          products.map((item)=>
+            <ProductCard item={item}/>
+          )
+        }
+      </div>
+      <Pagination 
+        sx={{ width: '100%', display: 'flex' ,alignItems: 'center', justifyContent: "center",textAlign: 'center' }}
+        count={products.length !==0 ? productSearch.page + 1 : productSearch.page} page={productSearch.page} renderItem={(item)=>(
+          <PaginationItem components={{previous: ArrowBackIcon, next: ArrowForwardIcon}} {...item}/>)
+        
+        } onChange ={paginationHanler}/>
     </div>
   
   )
--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,15 +1,10 @@
-import { Container, PaginationItem } from "@mui/material";
+import { Container } from "@mui/material";
 import Products from "../productsCard/Products";
-import Subheader from "./Subheader";
-import Pagination from "@mui/material/Pagination";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { Route, Switch } from "react-router-dom";
 import EachProdcut from "../eachProduct";
 
 export function ProductsPage() {
     return <Container>
-        <Subheader/>
         <Switch>
             <Route path={`/products/:productId`}>
                 <EachProdcut/>
@@ -17,13 +12,7 @@ export function ProductsPage() {
             <Route path={`/products`}>
                 <h2 className={"products-headline"}>Наши продукты</h2>
                 <Products/>
-                <Pagination 
-                    sx={{ width: '100%', display: 'flex' ,alignItems: 'center', justifyContent: "center",textAlign: 'center' }}
-                    count={3} page={1} renderItem={(item)=>(
-                        <PaginationItem components={{previous: ArrowBackIcon, next: ArrowForwardIcon}} {...item}
-                        />
-                )
-                }/>
+                
             </Route>
         </Switch>
     </Container>
